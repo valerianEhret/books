b8ea4938cb1c9a8fac5a9c37c9a45aae2bdddd63
valerianEhret <vkadirovv@gmail.com>
2021-10-03 23:12:29 +0200

    Add basic table in Authors and Books components

--- src/components/Authors.tsx
+++ src/components/Authors.tsx
@@ -1,10 +1,77 @@
 import React from "react";
+import {Button, Row, Table} from "antd";
+import { NavLink } from "react-router-dom";
 
 
 export const Authors: React.FC = () => {
+
+    const columns = [
+        {
+            title: 'FirstName',
+            dataIndex: 'firstName',
+            key: 'firstName',
+        },
+        {
+            title: 'SecondName',
+            dataIndex: 'secondName',
+            key: 'secondName',
+        },
+        {
+            title: 'More',
+            dataIndex: 'more',
+            key: 'address',
+        },
+        {
+            title: 'Edit',
+            key: 'edit',
+            dataIndex: 'edit',
+        },
+        {
+            title: 'Delete',
+            key: 'delete',
+            dataIndex: 'delete',
+        }
+    ]
+
+    const dataSource = [
+        {
+            key: 'key',
+            firstName: 'firstName',
+            secondName: 'secondName',
+            more: 'more',
+            edit: 'more',
+            delete: 'more'
+        },
+        {
+            key: 'key',
+            firstName: 'firstName',
+            secondName: 'secondName',
+            more: 'more',
+            edit: 'more',
+            delete: 'more'
+        },
+        {
+            key: 'key',
+            firstName: 'firstName',
+            secondName: 'secondName',
+            more: 'more',
+            edit: 'more',
+            delete: 'more'
+        },
+
+    ];
+
     return (
         <>
-            Authors
+            <div style={{margin: 20}}>
+                <Row justify={'end'}><Button type="link"><NavLink
+                    to={'/author/create'}>Create+</NavLink></Button>
+                </Row>
+                <Table  columns={columns}
+                        dataSource={dataSource}
+                />
+
+            </div>
         </>
     )
 }
--- src/components/Books.tsx
+++ src/components/Books.tsx
@@ -1,10 +1,95 @@
 import React from "react";
+import {Button, Row, Table} from "antd";
+import { NavLink } from "react-router-dom";
 
 
 export const Books: React.FC = () => {
+
+    const columns = [
+        {
+            title: 'Title',
+            dataIndex: 'title',
+            key: 'title',
+        },
+        {
+            title: 'AuthorLastName',
+            dataIndex: 'authorLastName',
+            key: 'authorLastName',
+        },
+        {
+            title: 'AuthorFirstName',
+            dataIndex: 'authorFirstName',
+            key: 'authorFirstName',
+        },
+        {
+            title:'Year',
+            dataIndex:'year',
+            key:'year'
+        },
+        {
+            title:'More',
+            dataIndex:   'more',
+            key:'more',
+
+        },
+        {
+            title: 'Edit',
+            key: 'edit',
+            dataIndex: 'edit',
+        },
+        {
+            title: 'Delete',
+            key: 'delete',
+            dataIndex: 'delete',
+        }
+    ];
+
+    const dataSource = [
+        {
+            key: 'key',
+            title:'title',
+            authorFirstName: 'authorFirstName',
+            authorLastName: 'authorLastName',
+            year:'year',
+            more: 'more',
+            edit: 'edit',
+            delete: 'delete'
+        },
+        {
+            key: 'key',
+            title:'title',
+            authorFirstName: 'authorFirstName',
+            authorLastName: 'authorLastName',
+            year:'year',
+            more: 'more',
+            edit: 'edit',
+            delete: 'delete'
+        },
+        {
+            key: 'key',
+            title:'title',
+            authorFirstName: 'authorFirstName',
+            authorLastName: 'authorLastName',
+            year:'year',
+            more: 'more',
+            edit: 'edit',
+            delete: 'delete'
+        },
+
+    ];
+
     return (
         <>
-            Books
+            <div style={{margin: 20}}>
+            <Row justify={'end'}><Button type="link"><NavLink
+                to={'/book/create'}>Create+</NavLink></Button>
+            </Row>
+                <Table  columns={columns}
+                        dataSource={dataSource}
+                />
+
+
+            </div>
         </>
     )
 }
